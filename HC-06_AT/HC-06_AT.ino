b260e25d06d1835333a03ead50e8751bd9503ee7
Dave <davide.gariselli@gmail.com>
2015-06-03 21:02:12 +0200

    initialization

---
+++ HC-06_AT/HC-06_AT.ino
@@ -0,0 +1,29 @@
+#include <SoftwareSerial.h>
+
+SoftwareSerial mySerial(11, 12); // TX HC-06 , RX HC-06 voltage divider
+
+void setup()  
+{
+ // Open serial communications and wait for port to open:
+  Serial.begin(9600);
+  
+  /*
+   while (!Serial) {
+    ; // wait for serial port to connect. Needed for Leonardo only
+  }
+  */
+  
+  Serial.println("Goodnight moon!");
+
+  // set the data rate for the SoftwareSerial port
+  mySerial.begin(9600);
+  mySerial.println("Hello, world?");
+}
+
+void loop() // run over and over
+{
+  if (mySerial.available())
+    Serial.write(mySerial.read());
+  if (Serial.available())
+    mySerial.write(Serial.read());
+}
---
+++ HC-06_blink_led_phone/HC-06_blink_led_phone.ino
@@ -0,0 +1,46 @@
+// RX arduino --> TX HC-06
+// TX arduino --> RX HC-06 con voltage divider
+
+int ledPin = 13;  // use the built in LED on pin 13 of the Uno
+int state = 0;
+int flag = 0;        // make sure that you return the state only once
+//int states = 0;
+
+void setup() {
+    // sets the pins as outputs:
+    pinMode(ledPin, OUTPUT);
+    digitalWrite(ledPin, LOW);
+
+    Serial.begin(9600); // Default connection rate for my BT module
+}
+
+void loop() {
+  
+    //if some data is sent, read it and save it in the state variable
+    if(Serial.available() > 0){
+    
+      state = Serial.read();
+      flag=0;
+    
+    if (state == 'a') {
+          Serial.println("Benveuto nel programma di prova MeArm del FabLab di Modena\n");
+        }
+        
+    // if the state is 0 the led will turn off
+    if (state == '0') {
+        digitalWrite(ledPin, LOW);
+        if(flag == 0){
+          Serial.println("LED: off");
+          flag = 1;
+        }
+    }
+    // if the state is 1 the led will turn on
+    else if (state == '1') {
+        digitalWrite(ledPin, HIGH);
+        if(flag == 0){
+          Serial.println("LED: on");
+          flag = 1;
+        }
+    }
+}
+}
