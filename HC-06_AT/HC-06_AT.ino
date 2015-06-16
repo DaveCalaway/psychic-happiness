3ec263d9e149b95895afc02cc9c6f3d0ee9995a6
Dave <davide.gariselli@gmail.com>
2015-06-16 19:48:15 +0200

    aggiornamento

--- HC-06_AT/HC-06_AT.ino
+++ HC-06_AT/HC-06_AT.ino
@@ -1,22 +1,22 @@
 #include <SoftwareSerial.h>
 
-SoftwareSerial mySerial(11, 12); // TX HC-06 , RX HC-06 voltage divider
+SoftwareSerial mySerial(6, 7); // TX HC-06 , RX HC-06 voltage divider
 
 void setup()  
 {
  // Open serial communications and wait for port to open:
-  Serial.begin(9600);
+  Serial.begin(9600); // velocità seriale
   
-  /*
+  
    while (!Serial) {
     ; // wait for serial port to connect. Needed for Leonardo only
   }
-  */
+  
   
   Serial.println("Goodnight moon!");
 
   // set the data rate for the SoftwareSerial port
-  mySerial.begin(9600);
+  mySerial.begin(19200); //velocità risposta BT
   mySerial.println("Hello, world?");
 }
 
